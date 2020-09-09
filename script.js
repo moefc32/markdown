001723f6e6d35ab2d85ecf3270f2f7d86caf05c1
Faizal Chan <moefc32@gmail.com>
2020-09-10 02:14:32 +0700

    update

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ new Vue({
     input: "# Hello there!"
   },
   computed: {
-    compiledMarkdown: function() {
-      return marked(this.input, {sanitize: true});
+    compiledMarkdown: function () {
+      return marked(this.input, {
+        sanitize: true
+      });
     }
   },
   methods: {
-    update: _.debounce(function(e) {
+    update: _.debounce(function (e) {
       this.input = e.target.value;
     }, 300)
   }
