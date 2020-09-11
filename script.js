464065433c0111e6265e86d336c1a408b6bc67ad
Faizal Chan <moefc32@gmail.com>
2020-09-11 19:45:51 +0700

    update

--- script.js
+++ script.js
@@ -23,7 +23,23 @@ new Vue({
   }
 });
 
-function savetoBrowser() {
+const downloadToFile = (content, filename, contentType) => {
+  const a = document.createElement('a');
+  const file = new Blob([content], {
+    type: contentType
+  });
+  a.href = URL.createObjectURL(file);
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(a.href);
+};
+
+function saveToBrowser() {
   b = document.getElementById("editor").value;
   localStorage.setItem('myItem', b);
 }
+
+function clickToDownload() {
+  const textArea = document.getElementById('editor');
+  downloadToFile(textArea.value, 'markdown.md', 'text/plain');
+};
